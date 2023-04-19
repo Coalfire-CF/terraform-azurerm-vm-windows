d7dad1fd3804144c16ee497649d3bf178013c238
Thomas Kennedy <101284566+tkennedy-cf@users.noreply.github.com>
2023-04-19 11:38:14 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -35,9 +35,9 @@ variable "availability_set_id" {
   default     = null
 }
 
-variable "availability_zones" {
+variable "availability_zone" {
   type        = string
-  description = "Specifies a list of Availability Zones in which the Windows VM should be located"
+  description = "Specifies an Availability Zone in which the Windows VM should be located"
   default     = null
 }
 
