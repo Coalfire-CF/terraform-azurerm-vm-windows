172ac6c60bd9810a07e4a7035b762441587e2aad
Keegan Flood <keegan.flood@coalfire.com>
2023-07-26 10:52:37 -0600

    change var to list(number)

--- variables.tf
+++ variables.tf
@@ -36,7 +36,7 @@ variable "availability_set_id" {
 }
 
 variable "availability_zone" {
-  type        = list
+  type        = list(number)
   description = "Specifies an Availability Zone in which the Windows VM should be located"
   default     = null
 }
