0903db591fe290816bb44eb7fde2baa6dc885fd3
Brandon Donahoo <brandon.donahoo@coalfire.com>
2025-04-02 14:02:14 -0400

    locals.tf typo

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   domain_join_command = (
-    "./ud_join_ad.sh -d \"${var.domain_join.domain_name}\" -dis \"${var.domain_join.disname}\" -a \"${var.domain_join.windows_admins_ad_group}\" -u \"${var.domain_join.user_name}\" -v \"${var.dj_kv_name}\" --host \"${azurerm_linux_virtual_machine.vm.name}\" -c \"${var.domain_join.azure_cloud}\";"
+    "./ud_join_ad.sh -d \"${var.domain_join.domain_name}\" -dis \"${var.domain_join.disname}\" -a \"${var.domain_join.windows_admins_ad_group}\" -u \"${var.domain_join.user_name}\" -v \"${var.dj_kv_name}\" --host \"${azurerm_windows_virtual_machine.vm.name}\" -c \"${var.domain_join.azure_cloud}\";"
   )
 
   commandToExecute = (
