683b19b4c27dad7c55153f7019e10004233cdab1
Brandon Donahoo <brandon.donahoo@coalfire.com>
2025-04-02 12:15:40 -0400

    add domain join switch cases

--- extensions.tf
+++ extensions.tf
@@ -16,6 +16,7 @@ resource "time_sleep" "wait_180_seconds" {
   ]
 }
 
+# SWITCH CASE 1: Join domain and install monitor agent, custom scripts
 resource "azurerm_virtual_machine_extension" "windows_domain_join" {
   count                      = var.is_domain_join ? 1 : 0
   name                       = "ud-dj"
@@ -40,6 +41,29 @@ resource "azurerm_virtual_machine_extension" "windows_domain_join" {
 
   protected_settings = jsonencode({
     "commandToExecute" = "./ud_linux_join_ad.sh -d \"${var.domain_join.domain_name}\" -dis \"${var.domain_join.disname}\" -a \"${var.domain_join.linux_admins_ad_group}\" -u \"${var.domain_join.user_name}\" -v \"${local.dj_kv_name}\" --host \"${azurerm_linux_virtual_machine.vm.name}\" -c \"${var.domain_join.azure_cloud}\";./ud_linux_monitor_agent.sh -w \"${data.azurerm_log_analytics_workspace.log_analytics.workspace_id}\" -s \"${data.azurerm_log_analytics_workspace.log_analytics.primary_shared_key}\";${chomp(var.custom_scripts)} exit 0"
+    "managedIdentity"  = {}
+  })
+}
+
+# SWITCH CASE 2: Join domain and install monitor agent, custom scripts
+resource "azurerm_virtual_machine_extension" "ud_monitor_agent" {
+  count                      = var.is_domain_join ? 0 : 1
+  name                       = "ud-dj"
+  virtual_machine_id         = azurerm_linux_virtual_machine.vm.id
+  publisher                  = "Microsoft.Azure.Extensions"
+  type                       = "CustomScript"
+  auto_upgrade_minor_version = "true"
+  type_handler_version       = "2.1"
+  depends_on = [
+    time_sleep.wait_180_seconds
+  ]
+
+  settings = jsonencode({
+    "fileUris" = concat([var.linux_monitor_agent_url], var.custom_scripts_fileUris)
+  })
+
+  protected_settings = jsonencode({
+    "commandToExecute" = "./ud_linux_monitor_agent.sh -w \"${data.azurerm_log_analytics_workspace.log_analytics.workspace_id}\" -s \"${data.azurerm_log_analytics_workspace.log_analytics.primary_shared_key}\";${chomp(var.custom_scripts)}; exit 0"
     "managedIdentity"  = {}
   })
 }
