ff8bd212e541af4d0b434dbc8c8b53e4d3701513
Brandon Donahoo <brandon.donahoo@coalfire.com>
2025-04-02 12:13:47 -0400

    move diag extensions to extension file

--- extensions.tf
+++ extensions.tf
@@ -43,3 +43,30 @@ resource "azurerm_virtual_machine_extension" "windows_domain_join" {
     "managedIdentity"  = {}
   })
 }
+
+resource "azurerm_virtual_machine_extension" "vm_network_watcher" {
+  name                       = "${var.vm_name}-network-watcher"
+  virtual_machine_id         = azurerm_windows_virtual_machine.vm.id
+  publisher                  = "Microsoft.Azure.NetworkWatcher"
+  type                       = "NetworkWatcherAgentWindows"
+  type_handler_version       = "1.4"
+  auto_upgrade_minor_version = true
+  depends_on                 = [azurerm_virtual_machine_extension.diagnostics]
+}
+
+resource "azurerm_virtual_machine_extension" "diagnostics" {
+  name                       = "${var.vm_name}-diagnostics-extension"
+  publisher                  = "Microsoft.Azure.Diagnostics"
+  type                       = "IaaSDiagnostics"
+  type_handler_version       = "1.16"
+  auto_upgrade_minor_version = "true"
+  virtual_machine_id         = azurerm_windows_virtual_machine.vm.id
+  settings                   = templatefile("${path.module}/shellscripts/winDiagnostics.json", { resource_id = azurerm_windows_virtual_machine.vm.id, storage_name = var.storage_account_vmdiag_name })
+
+  protected_settings = <<SETTINGS
+  {
+    "managedIdentity": {},
+    "storageAccountName": "${var.storage_account_vmdiag_name}"
+  }
+SETTINGS
+}
