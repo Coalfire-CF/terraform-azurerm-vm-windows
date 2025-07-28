84f02a5ef42e0de0bea26eacb6791a4a77a444ec
Andy Mills <andy.mills@coalfire.com>
2025-07-28 05:38:56 -0400

    fix: swap linux to windows

--- backup.tf
+++ backup.tf
@@ -2,7 +2,7 @@ module "vm_backup" {
   source              = "git::https://github.com/Coalfire-CF/terraform-azurerm-vm-backup.git?ref=v0.0.1"
   resource_group_name = var.resource_group_name
   recovery_vault_name = var.recovery_vault_name
-  vm_id               = azurerm_linux_virtual_machine.vm.id
+  vm_id               = azurerm_windows_virtual_machine.vm.id
   policy_name         = local.backup_policy_name
   start_time          = "05:00" # midnight EST
   enable_backup       = true
--- site-recovery.tf
+++ site-recovery.tf
@@ -2,7 +2,7 @@ module "site_recovery" {
   source = "git::https://github.com/Coalfire-CF/terraform-azurerm-vm-site-recovery.git?ref=v0.0.1"
 
   enable_site_recovery = true
-  vm_id                = azurerm_linux_virtual_machine.vm.id
+  vm_id                = azurerm_windows_virtual_machine.vm.id
   vm_name              = var.vm_name
   recovery_vault_name  = var.recovery_vault_name
   recovery_vault_rg    = var.recovery_vault_rg
